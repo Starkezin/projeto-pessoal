9a6990b9dc740b6d1310627e322a630842ea6c6c
Samuel Starke <samuel_starke@hotmail.com>
2026-08-17 17:49:51 -0300

    adiciona scripts `lint:check e lint:fix`.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # projeto-pessoal
+
 Implementação de um projeto pessoal junto do curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1> Amar você </h1>
+function Home() {
+  return <h1> Amar você </h1>;
 }
 
 export default Home;
